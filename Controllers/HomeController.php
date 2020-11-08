7d8f401ac132e842b73aaaded5f9eb83da5618be
Gianni Ricciardi <gianniricciardi3779@gmail.com>
2020-11-07 23:40:28 -0300

    Update HomeController.php
    Update of the Home Controller

---
+++ Controllers/HomeController.php
@@ -0,0 +1,235 @@
+<?php
+    namespace Controllers;
+
+    /*Controllers*/
+
+    use Controllers\UserController as UserController;
+    use Controllers\CinemaController as CinemaController;
+    use Controllers\RoomController as RoomController;
+    use Controllers\MovieController as MovieController;
+    use Controllers\GenreController as GenreController;
+    use Controllers\ShowController as ShowController;
+    use Controllers\PurchaseController as PurchaseController;
+
+    class HomeController
+    {
+
+        public function __construct(){
+
+        }
+ 
+        public function Index($message = ""){
+            require_once(VIEWS_PATH."home.php");
+        }      
+
+        public function Register(){
+            require_once(VIEWS_PATH."home-register.php");
+        }
+
+
+        public function CinemasView( $validMessage = null ){
+
+            $userController = new UserController();
+            $cinemaController = new CinemaController();
+            
+            $user = $userController->checkSession();
+            if($user){
+
+                $cinemasList = $cinemaController->GetAll();
+                require_once(VIEWS_PATH."admin-cinemas.php");
+
+            }else{
+                $userController->Logout();
+            }
+        }
+
+        public function RoomsView( $validMessage = null ){
+
+            $userController = new UserController();
+            $roomController = new RoomController();
+            $cinemaController = new CinemaController();
+
+            $user = $userController->checkSession();
+            
+        
+            if($user){
+                $roomsList = $roomController->GetAll();
+                $cinemasList = $cinemaController->GetAll();
+                require_once(VIEWS_PATH."admin-rooms.php");
+            }else{
+                $userController->Logout();
+            }
+        }
+
+        public function ShowsViewAdmin( $validMessage = null ){
+
+            $userController = new UserController();
+            $movieController = new MovieController();
+            $roomController = new RoomController();
+            $showController = new ShowController();
+            $cinemaController = new CinemaController();
+            
+
+            $user = $userController->checkSession();
+            
+            if($user){
+                $moviesList = $movieController->GetMovies();
+                $genresList = $movieController->GetGenres();
+                $roomsList = $roomController->GetAll();
+                $showsList = $showController->GetTable();
+
+                require_once(VIEWS_PATH."admin-shows.php");
+            }else{
+                $userController->Logout();
+            }
+        }
+
+        public function InfoViewAdmin( $validMessage = null ){
+
+            $userController = new UserController();
+            $movieController = new MovieController();
+            $roomController = new RoomController();
+            $showController = new ShowController();
+            $cinemaController = new CinemaController();
+            $purchaseController = new PurchaseController();
+
+            $user = $userController->checkSession();
+            
+            if($user){
+
+                $moviesList = $movieController->GetMovies();
+                
+                $showsList = $showController->GetTable();
+
+                $purchasesList = $purchaseController->GetAll();
+
+                require_once(VIEWS_PATH."admin-table-info.php");
+            }else{
+                $userController->Logout();
+            }
+        }
+
+
+
+        public function ShowsViewClient(){
+
+            $userController = new UserController();
+            $movieController = new MovieController();
+            $roomController = new RoomController();
+            $showController = new ShowController();
+
+            $user = $userController->checkSession();
+            
+            if($user){
+
+                $moviesList = $movieController->GetMovies();
+                $genresList = $movieController->GetGenres();
+                
+                $roomsList = $roomController->GetAll();
+                $showsList = $showController->GetAll();  
+
+            
+                require_once(VIEWS_PATH."client-shows-list.php");
+            }else{
+                $userController->Logout();
+            }
+        }
+
+        public function MovieDescription($id_show){
+
+            $userController = new UserController();
+            $movieController = new MovieController();
+            $roomController = new RoomController();
+            $showController = new ShowController();
+
+            $user = $userController->checkSession();
+            
+            if($user){
+                $moviesList = $movieController->GetMovies();
+                $genresList = $movieController->GetGenres();
+                
+                $roomsList = $roomController->GetAll();
+                $show = $showController->GetById($id_show);  
+            
+                require_once(VIEWS_PATH."client-movie-description.php");
+            }else{
+                $userController->Logout();
+            }
+        }
+
+
+        public function MovieListByGenre($id){
+
+            $userController = new UserController();
+            $movieController = new MovieController();
+            $roomController = new RoomController();
+            $showController = new ShowController();
+
+            $user = $userController->checkSession();
+            
+            if($user){
+
+                $moviesList = $movieController->MovieListViewForGenre($id);
+                $genresList = $movieController->GetGenres();
+                
+                $roomsList = $roomController->GetAll();
+                $showsList = $showController->GetAll();  
+            
+                require_once(VIEWS_PATH."client-shows-list.php");
+            }else{
+                $userController->Logout();
+            }
+        }
+
+        public function MovieListByDate($day){
+
+            $userController = new UserController();
+            $movieController = new MovieController();
+            $roomController = new RoomController();
+            $showController = new ShowController();
+
+            $user = $userController->checkSession();
+            
+            if($user){
+
+                $moviesList = $movieController->GetMovies();
+                $genresList = $movieController->GetGenres();
+                
+                $roomsList = $roomController->GetAll();
+                $showsList = $showController->getMovieByDate($day);  
+            
+                require_once(VIEWS_PATH."client-shows-list.php");
+            }else{
+                $userController->Logout();
+            }
+        }
+
+        public function PurchasesList(){
+
+            $userController = new UserController();
+            $movieController = new MovieController();
+            $roomController = new RoomController();
+            $showController = new ShowController();
+
+            $user = $userController->checkSession();
+            
+            if($user){
+
+                $moviesList = $movieController->GetMovies();
+                $genresList = $movieController->GetGenres();
+                
+                $roomsList = $roomController->GetAll();
+                $showsList = $showController->GetAll();  
+            
+                require_once(VIEWS_PATH."client-shows-list.php");
+            }else{
+                $userController->Logout();
+            }
+        }
+
+
+
+    
+    }
+
+?>
