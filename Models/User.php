323d52fb13b75245b611aa097b8e0cae780cfcbb
Gianni Ricciardi <gianniricciardi3779@gmail.com>
2020-11-08 16:36:05 -0300

    User.php
    Update of User class

--- Models/User.php
+++ Models/User.php
@@ -1,22 +1,38 @@
 <?php
 namespace Models;
 
-include("PerfilUser.php");
+use Models\Rol as Rol;
 
-class User extends PerfilUser{
+class User{
 
-    private $email;       //string
-    private $password;   //string
-    private $rol;    
+    private $email;    
+    private $password;   
+    private $exist;
 
+    private $rol;   
+
+
+    //Getters
+    public function getId(){
+        return $this->id;
+    }
     public function getEmail(){
         return $this->email;
     }
     public function getPassword(){
         return $this->password;
     }
+    public function getExist(){
+        return $this->exist;
+    }
+
     public function getRol(){
         return $this->rol;
+    }
+
+    //Setters
+    public function setId($id){
+        $this->id = $id;
     }
     public function setEmail($email){
         $this->email = $email;
@@ -24,11 +40,11 @@ class User extends PerfilUser{
     public function setPassword($password){
         $this->password = $password;
     }
+
     public function setRol($rol){
         $this->rol = $rol;
     }
+
 }
-
-
 
 ?>
