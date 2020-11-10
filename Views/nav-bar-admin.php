899f762e7b102a46e6ddcbfd219b7f34033ac6eb
Tomas <tom.ia@hotmail.com>
2020-11-10 17:53:31 -0300

    cambio nav 2

--- Views/nav-bar-admin.php
+++ Views/nav-bar-admin.php
@@ -17,12 +17,12 @@
       <!-- Divider -->
       <hr class="sidebar-divider my-0">
 
-      <!-- Nav Item - Dashboard -->
+      <!-- Nav Item - Dashboard 
       <li class="nav-item active">
         <a class="nav-link" href="#">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
-      </li>
+      </li>-->
 
       <!-- Divider -->
       <hr class="sidebar-divider">
