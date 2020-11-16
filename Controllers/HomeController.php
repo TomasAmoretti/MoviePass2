cd98e6e637fecc6161d3f20191a946b16501bd19
Gianni Ricciardi <gianniricciardi3779@gmail.com>
2020-11-16 20:44:32 -0300

    Corrección Home Controller
    Corecciones en base a las nuevas vistas de invitados.

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -19,8 +19,31 @@
         }
  
         public function Index($message = ""){
+<<<<<<< Updated upstream
             require_once(VIEWS_PATH."home.php");
+=======
+            $userController = new UserController();
+            $movieController = new MovieController();
+            $roomController = new RoomController();
+            $showController = new ShowController();
+            
+            $moviesList = $movieController->GetMovies();
+            $genresList = $movieController->GetGenres();
+                
+            $roomsList = $roomController->GetAll();
+            $showsList = $showController->GetAll();
+            $user = $userController->checkSession();
+            if($user){
+                $userController->Login($user->getUser()->getEmail(), $user->getUser()->getPassword());
+            }else{
+                require_once(VIEWS_PATH."home.php");
+            }
+>>>>>>> Stashed changes
         }      
+
+        public function User(){
+            require_once(VIEWS_PATH."login.php");
+        }
 
         public function Register(){
             require_once(VIEWS_PATH."home-register.php");
@@ -119,6 +142,7 @@
             $showController = new ShowController();
 
             $user = $userController->checkSession();
+<<<<<<< Updated upstream
             
             if($user){
 
@@ -127,11 +151,24 @@
                 
                 $roomsList = $roomController->GetAll();
                 $showsList = $showController->GetAll();  
+=======
+>>>>>>> Stashed changes
 
+            $moviesList = $movieController->GetMovies();
+            $genresList = $movieController->GetGenres();
+            $roomsList = $roomController->GetAll();
+            $showsList = $showController->GetAll();
             
+<<<<<<< Updated upstream
                 require_once(VIEWS_PATH."client-shows-list.php");
             }else{
                 $userController->Logout();
+=======
+            if($user){
+                require_once(VIEWS_PATH."client-show-list.php");
+            }else{
+                require_once(VIEWS_PATH."home.php");
+>>>>>>> Stashed changes
             }
         }
 
@@ -143,6 +180,7 @@
             $showController = new ShowController();
 
             $user = $userController->checkSession();
+<<<<<<< Updated upstream
             
             if($user){
                 $moviesList = $movieController->GetMovies();
@@ -150,10 +188,22 @@
                 
                 $roomsList = $roomController->GetAll();
                 $show = $showController->GetById($id_show);  
+=======
+
+            $moviesList = $movieController->GetMovies();
+            $genresList = $movieController->GetGenres();
+            $roomsList = $roomController->GetAll();
+            $show = $showController->GetById($id_show);
+>>>>>>> Stashed changes
             
+            if($user){
                 require_once(VIEWS_PATH."client-movie-description.php");
             }else{
+<<<<<<< Updated upstream
                 $userController->Logout();
+=======
+                require_once(VIEWS_PATH."guess-movie-description.php");
+>>>>>>> Stashed changes
             }
         }
 
@@ -166,18 +216,28 @@
             $showController = new ShowController();
 
             $user = $userController->checkSession();
+<<<<<<< Updated upstream
             
             if($user){
+=======
+>>>>>>> Stashed changes
 
-                $moviesList = $movieController->MovieListViewForGenre($id);
-                $genresList = $movieController->GetGenres();
+            $moviesList = $movieController->MovieListViewForGenre($id);
+            $genresList = $movieController->GetGenres();
                 
-                $roomsList = $roomController->GetAll();
-                $showsList = $showController->GetAll();  
+            $roomsList = $roomController->GetAll();
+            $showsList = $showController->GetAll();  
             
+<<<<<<< Updated upstream
                 require_once(VIEWS_PATH."client-shows-list.php");
             }else{
                 $userController->Logout();
+=======
+            if($user){
+                require_once(VIEWS_PATH."client-show-list.php");
+            }else{
+                require_once(VIEWS_PATH."home.php");
+>>>>>>> Stashed changes
             }
         }
 
@@ -190,6 +250,7 @@
 
             $user = $userController->checkSession();
             
+<<<<<<< Updated upstream
             if($user){
 
                 $moviesList = $movieController->GetMovies();
@@ -204,6 +265,22 @@
             }
         }
 
+=======
+            $moviesList = $movieController->GetMovies();
+            $genresList = $movieController->GetGenres();
+                
+            $roomsList = $roomController->GetAll();
+            $showsList = $showController->getMovieByDate($day);
+
+            if($user){
+                require_once(VIEWS_PATH."client-show-list.php");
+            }else{
+                require_once(VIEWS_PATH."home.php");
+            }
+        }
+
+
+>>>>>>> Stashed changes
         public function PurchasesList(){
 
             $userController = new UserController();
@@ -226,9 +303,6 @@
                 $userController->Logout();
             }
         }
-
-
-
     
     }
 
