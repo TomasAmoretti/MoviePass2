36722b293543e597dba3c2328c1c7d937db979ff
DanteSan <dante_GH@hotmail.com.ar>
2020-11-17 19:17:15 -0300

    Arregladas validaciones

--- Controllers/ShowController.php
+++ Controllers/ShowController.php
@@ -86,7 +86,7 @@ class ShowController
                     array_push($validShows, $show);
                 }
             }
-            $this->validateHourAndDay($validShows);
+            $this->validateHourAndDayShow($validShows);
             $message = $validMessage;
 
             return  $validShows;
@@ -105,8 +105,7 @@ class ShowController
 
         try {
 
-            $this->validateHour($day, $hour, $id_movie, $id_room);
-
+            $message = $this->validateShow($day, $hour, $id_movie, $id_room);
 
             $show = new Show();
 
@@ -120,7 +119,7 @@ class ShowController
 
             $this->showDAO->Add($show);
 
-            $this->homeController->ShowsViewAdmin();
+            $this->homeController->ShowsViewAdmin($message);
         } catch (Exception $ex) {
             $message = $ex->getMessage();
             $this->homeController->ShowsViewAdmin($message);
@@ -158,44 +157,49 @@ class ShowController
         }
     }
 
-    public function validateHour($day, $hour, $idMovie, $id_room)
+    public function validateShow($day, $hour, $idMovie, $id_room)
     {
+        if($this->horarioLaboral($idMovie, $hour)){
 
-        $roomList = $this->roomDAO->GetAll();
-        $hrs = $this->hourToDecimal($hour); //A que hora empieza la pelicula que quiero cargar
-        $newMovie = $this->movieDuration($idMovie, $hrs); //A que hora termina la pelicula que quiero cargar
-        $showList = $this->showDAO->GetTable();
-        
+            $roomList = $this->roomDAO->GetAll();
+            $hrs = $this->hourToDecimal($hour); //A que hora empieza la pelicula que quiero cargar
+            var_dump($hrs);
+            $newMovie = $this->movieDuration($idMovie, $hrs); //A que hora termina la pelicula que quiero cargar
+            $showList = $this->showDAO->GetTable();
 
-        foreach ($showList as $show) {
+            foreach ($showList as $show) {
 
-            foreach ($roomList as $room) {
+                foreach ($roomList as $room) {
 
-                if (($room['cinema_name'] == $show['cinema_name']) && ($show['state'] == 1)) {
+                    if (($room['cinema_name'] == $show['cinema_name']) && ($show['state'] == 1)) {
 
-                    if (($day == $show['day']) && ($hour != $show['hour']) && ($idMovie == $show['id_movie']) && ($show['state'] == 1)) {
-                        throw new PDOException("Esta pelicula ya esta cargada en este cine y en este dia!");
-                    }
-                    if (($id_room == $show['id_room']) && ($show['state'] == 1)) {
+                        if (($day == $show['day']) && ($hour != $show['hour']) && ($idMovie == $show['id_movie']) && ($show['state'] == 1)) {
+                            throw new PDOException("Esta pelicula ya esta cargada en este cine y en este dia!");
+                        }
+                        if (($id_room == $show['id_room']) && ($show['state'] == 1)) {
 
-                        if ($day == $show['day']) {
+                            if ($day == $show['day']) {
 
-                            $showHour = $this->hourToDecimal($show['hour']); //A que hora empieza la pelicula que esta en la cartelera en el mismo dia
-                            $endMovie = $this->movieDuration($show['id_movie'], $showHour); //A que hora termina la pelicula que esta en la cartelera en el mismo dia
+                                $showHour = $this->hourToDecimal($show['hour']); //A que hora empieza la pelicula que esta en la cartelera en el mismo dia
+                                $endMovie = $this->movieDuration($show['id_movie'], $showHour); //A que hora termina la pelicula que esta en la cartelera en el mismo dia
 
-                            if ($newMovie < $showHour) {
-                                return $message = "La pelicula fue cargada con exito";
-                                //throw new PDOException("La pelicula fue cargada con exito");
-                            } elseif ($hrs > $endMovie) {
-                                return $message = "La pelicula fue cargada con exito";
-                                //throw new PDOException("La pelicula fue cargada con exito");
-                            } else {
-                                throw new PDOException("El horario no esta disponible!");
+                                if ($newMovie < $showHour) {
+                                    return $message = "La pelicula fue cargada con exito";
+
+                                } elseif ($hrs > $endMovie) {
+                                    return $message = "La pelicula fue cargada con exito";
+                                        
+                                } else {
+                                    throw new PDOException("El horario no esta disponible!");
+                                }
                             }
                         }
                     }
                 }
             }
+        }
+        else{
+            throw new PDOException("El horario ingresado esta fuera del horario laboral");
         }
     }
 
@@ -217,7 +221,7 @@ class ShowController
         return (($hms[0] * 60) + $hms[1]);
     }
 
-    private function validateHourAndDay($showsList)
+    private function validateHourAndDayShow($showsList)
     {
         date_default_timezone_set("America/Argentina/Buenos_Aires"); //setea a la zona horaria correspondiente (por defecto viene en GMT)
         $date = getdate();
@@ -225,16 +229,30 @@ class ShowController
         $hourLocale = (($date['hours'] * 60) + ($date['minutes']));
 
         foreach ($showsList as $show) {
-
-            if ($show['day'] <= date('Y-m-d')) {
-
+            
+            if ($show['day'] == date('Y-m-d')) {
                 $hourShow = $this->hourToDecimal($show['hour']);
                 if ($hourShow < $hourLocale) {
                     if ($show['state']) {
                         $this->Remove($show['id_show']);
                     }
                 }
+            }if(($show['day'] < date('Y-m-d')) && ($show['state'])){
+                $this->Remove($show['id_show']);
             }
+        }
+    }
+
+    private function horarioLaboral($idMovie, $hour){
+
+        $startHour = 900;//comienzo del horario de proyeccion
+        $endHour = 1439;//fin del horario de proyeccion
+        $startShowHour = $this->hourToDecimal($hour);//comienzo de la pelicula
+        $endShowHour = $this->movieDuration($idMovie, $startShowHour);//fin de la pelicula
+        if(($startShowHour >= $startHour) && ($endShowHour <= $endHour)){
+            return true;
+        }else{
+            return false;
         }
     }
 }
