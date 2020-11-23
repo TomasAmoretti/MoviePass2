5aa1074ee18514c563f1d0dc0d22bcd8d96fbbdf
Gianni Ricciardi <gianniricciardi3779@gmail.com>
2020-11-22 23:56:12 -0300

    header
    Corrección espacios innecesarios y falla mínima.

--- Views/header.php
+++ Views/header.php
@@ -16,16 +16,20 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Oswald:300,400,500,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
-    
+
     <!-- Custom styles for this template-->
     <link href="<?php echo CSS_PATH; ?>home.min.css" rel="stylesheet">
-    
+    <link href="<?php echo CSS_PATH; ?>home.css" rel="stylesheet">
+
     <link href="<?php echo CSS_PATH; ?>dot-icons.css" rel="stylesheet" type="text/css">
     <link href="<?php echo CSS_PATH; ?>theme.css" rel="stylesheet" type="text/css">
 
     <link href="<?php echo CSS_PATH; ?>movie.css" rel="stylesheet">
 
-    <link href="<?php echo VENDOR_PATH ;?>/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="<?php echo VENDOR_PATH ;?>datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <link href="<?php echo VENDOR_PATH ;?>slick/slick.css" rel="stylesheet">
+    <link href="<?php echo VENDOR_PATH ;?>slick/slick-theme.css" rel="stylesheet">
 
   </head>
  <body class="bg-gradient-primary" id="page-top"> 
